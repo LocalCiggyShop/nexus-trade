7085c394ac9afdbb5d31535d4938c70492d83b5c
LocalCiggyShop <64781068+LocalCiggyShop@users.noreply.github.com>
2025-12-23 21:33:41 +1100

    Edit key

--- src/lib/supabase.ts
+++ src/lib/supabase.ts
@@ -1,7 +1,6 @@
 import { createClient } from '@supabase/supabase-js'
 
 const supabaseUrl = "https://ehkdxygrzinrzheipaia.supabase.co"
-const supabaseAnonKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImVoa2R4eWdyemlucnpoZWlwYWlhIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjUyNzU2ODYsImV4cCI6MjA4MDg1MTY4Nn0.w5KFieJBXsmh2R-qXLu9gqaCxuBFotHvyMo1gHaeiek"
 
 export const supabase = createClient(supabaseUrl, supabaseAnonKey, {
   auth: {
